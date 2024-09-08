59654c7e6fecad35ddf62a32874f755db7ec3239
IamBananana <richardgjelland@hotmail.com>
2024-09-08 14:26:33 +0200

    added comments

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+/**
+ * @author Richie!
+ * display is always linked to element "display", hence "const"
+ */
 const display = document.getElementById("display");
 
+/**
+ * 
+ * @param {*} input the input is recieved from <input> html and appends
+ *                  to the display.value
+ */
 function appendToDisplay(input) {
     display.value += input;
 }
 
+//set display to empty
 function clearDisplay() {
     display.value = "";
 }
 
+//get the value from display and use the inbuilt eval() method. This method
+//sucks and works as a template for now. Also if error we type error to display
 function calculate() {
     try {
         display.value = eval(display.value);
@@ -16,6 +28,7 @@ function calculate() {
         display.value = "Error";
     }
 }
+//small check to see if you start with a comma which is not possible
 function commaCheck() {
     if (display.value == "") {
         display.value = "Brrrrr";
@@ -23,6 +36,7 @@ function commaCheck() {
         appendToDisplay(",");
     }
 }
+//SQUIRT
 function squirt() {
     display.value = "Squirt";
 }
